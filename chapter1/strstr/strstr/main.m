c347268cac8189b4c2e2cc07494b61c4e803da37
Tianji Li <skyahead@gmail.com>
2015-01-10 11:15:45 -0500

    chapter 1, question 5: strstr

---
+++ chapter1/strstr/strstr/main.m
@@ -0,0 +1,38 @@
+//
+//  main.m
+//  strstr
+//
+//  Created by Tianji Li on 2015-01-10.
+//  Copyright (c) 2015 Tianji Li. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int strStr(NSString *needle, NSString *haystack)
+{
+    NSUInteger needleSize = [needle length];
+    NSUInteger haystackSize = [haystack length];
+    
+    for (NSUInteger i=0; ; i++) {
+        for (NSUInteger j=0; ; j++){
+            
+            if ( j == needleSize )
+                return (int)i;
+
+            if ( i + j == haystackSize )
+                return (int)-1;
+            
+            if ( [haystack characterAtIndex:i + j] != [needle characterAtIndex:j] )
+                break;
+        }
+    }
+}
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSString *needle = @"a";
+        NSString *haystack = @"ab";
+        NSLog(@"needle=%@, haystack=%@, strstr=%d", needle, haystack, strStr(needle, haystack));
+    }
+    return 0;
+}
