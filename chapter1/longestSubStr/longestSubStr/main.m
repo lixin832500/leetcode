f2117cef01fe6f1a019cac550fcaf0cb6df1fa82
Tianji Li <skyahead@gmail.com>
2015-01-24 12:50:44 -0500

    chapter 1: question 10, longest unique sub string

---
+++ chapter1/longestSubStr/longestSubStr/main.m
@@ -0,0 +1,85 @@
+//
+//  main.m
+//  longestSubStr
+//
+//  Created by Tianji Li on 2015-01-24.
+//  Copyright (c) 2015 Tianji Li. All rights reserved.
+//
+
+/**
+ Question:
+ Given a string, find the length of the longest substring without repeating characters. For
+ example, the longest substring without repeating letters for “abcabcbb” is “abc”, which
+ the length is 3. For “bbbbb” the longest substring is “b”, with the length of 1.
+ */
+
+#import <Foundation/Foundation.h>
+
+BOOL isUnique(NSString *s)
+{
+    NSMutableDictionary *uniqueMap = @{}.mutableCopy;
+    
+    for (NSUInteger i=0; i<[s length]; i++) {
+        NSString *c = [s substringWithRange:NSMakeRange(i, 1)];
+        if ( [uniqueMap objectForKey:c] )
+            return NO;
+        else {
+            [uniqueMap setObject:c forKey:c];
+        }
+    }
+    
+    return YES;
+}
+
+NSUInteger longstSubStr(NSString *s)
+{
+    NSUInteger subStrLen = 0;
+    NSUInteger lenS = [s length];
+    
+    // sanity check
+    if ( 0 == lenS ) return subStrLen;
+    
+    for (NSUInteger i=lenS; i>0; i--) {
+        for(NSUInteger j=0; j<=(lenS - i); j++) {
+            NSString *subS = [s substringWithRange:NSMakeRange(j, i)];
+            if ( isUnique(subS) ) {
+                subStrLen = [subS length];
+                return subStrLen;
+            }
+        }
+    }
+
+    return subStrLen;
+}
+
+NSUInteger lenOfLongestStr(NSString *s)
+{
+    NSMutableDictionary *charMap = @{}.mutableCopy;
+    NSInteger lenS = [s length];
+    NSInteger maxLen = 0;
+    NSInteger start = 0;
+    for ( NSInteger end = 0; end < lenS; end++) {
+        NSString *c = [s substringWithRange:NSMakeRange(end, 1)];
+        NSNumber *cPos = [charMap objectForKey:c];
+
+        if ( cPos && [cPos integerValue] >= start ) {
+            NSLog(@"%lu, %lu", [cPos integerValue], start);
+            start = [cPos integerValue] + 1;
+        }
+        
+        [charMap setObject:[[NSNumber alloc] initWithInteger:end] forKey:c];
+        
+        maxLen  = MAX(maxLen, end - start + 1);
+    }
+    
+    return maxLen;
+}
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSString *s = @"aabcdaaegfhpi";
+        NSLog(@"%lu", lenOfLongestStr(s));
+        NSLog(@"%lu", longstSubStr(s));
+    }
+    return 0;
+}
