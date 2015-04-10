79d98c64ded69bffb753be76d48d87e10405d53f
Tianji Li <skyahead@gmail.com>
2015-04-09 22:18:16 -0400

    validParentheses

---
+++ online/validParentheses/validParentheses/main.m
@@ -0,0 +1,26 @@
+//
+//  main.m
+//  validParentheses
+//
+//  Created by Tianji Li on 2015-02-11.
+//  Copyright (c) 2015 Tianji Li. All rights reserved.
+//
+
+/**
+
+ https://oj.leetcode.com/problems/valid-parentheses/
+ 
+ Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ 
+ The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+ 
+ */
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
