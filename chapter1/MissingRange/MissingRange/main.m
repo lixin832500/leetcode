eb5a1a36049e91403622e67b93d45b89290c2001
crystalztl <zhangtianli7051@163.com>
2016-12-18 11:30:06 +0800

    logic fix for missingRange

--- chapter1/MissingRange/MissingRange/main.m
+++ chapter1/MissingRange/MissingRange/main.m
@@ -19,23 +19,22 @@ NSArray *findMissingRange(NSArray *intArray){
     NSMutableArray *missingRange = @[].mutableCopy;
     NSUInteger count = intArray.count;
     if (intArray.count == 0) {
-        NSNull *null = [[NSNull alloc]init];
-        return @[null];
+        return @[];
     }
     
     
     if (![intArray[0]isEqualToNumber:@0]) {
-        if ([intArray[1] isEqualToNumber:@1]) {
+        if ([intArray[0] isEqualToNumber:@1]) {
             [missingRange addObject:@"0"];
         }else{
-            [missingRange addObject:[NSString stringWithFormat:@"0->%d",[intArray[1] intValue]-1]];
+            [missingRange addObject:[NSString stringWithFormat:@"0->%d",[intArray[0] intValue]-1]];
         }
     }
     
     
-    for (NSUInteger i=2; i<intArray.count; i++) {
+    for (NSUInteger i=1; i<intArray.count; i++) {
         int diff = [intArray[i] intValue] - [intArray[i-1] intValue];
-        if ( diff!=1) {
+        if (diff!=1) {
             if (diff == 2) {
                 [missingRange addObject:[NSString stringWithFormat:@"%d",[intArray[i] intValue] - 1]];
             }else{
@@ -48,7 +47,7 @@ NSArray *findMissingRange(NSArray *intArray){
         if ([[intArray lastObject] isEqualToNumber:@98]) {
             [missingRange addObject:@"99"];
         }else{
-            [missingRange addObject:[NSString stringWithFormat:@"%d->99",[intArray[count-2] intValue] + 1]];
+            [missingRange addObject:[NSString stringWithFormat:@"%d->99",[intArray[count-1] intValue] + 1]];
         }
     }
     
@@ -58,6 +57,15 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
        
         NSArray *intArray = @[@2,@4,@6,@78];
+        NSLog(@"%@",[findMissingRange(intArray) description]);
+        
+        intArray = @[@0,@4,@6,@99];
+        NSLog(@"%@",[findMissingRange(intArray) description]);
+        
+        intArray = @[@8,@9,@88,@98];
+        NSLog(@"%@",[findMissingRange(intArray) description]);
+        
+        intArray = @[];
         NSLog(@"%@",[findMissingRange(intArray) description]);
     }
     return 0;
